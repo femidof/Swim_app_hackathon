42ecce0f79eca386b9b8ff6a4990410e2ee8041e
marrodri <46203279+marrodri@users.noreply.github.com>
2020-08-01 16:07:11 -0500

    setting pool section screen and pool posts

--- lib/screens/pool_section_chat.dart
+++ lib/screens/pool_section_chat.dart
@@ -29,6 +29,23 @@ class _PoolSectionChatState extends State<PoolSectionChat> {
             children: <Widget>[
               Text("Posts"),
               PoolPosts(),
+              // Container(
+              //   child: ListView.builder(
+              //     //spacing to the left
+              //     padding: EdgeInsets.only(left:10.0),
+              //     itemCount: 5,
+              //     itemBuilder: (BuildContext context, int index) {
+              //       return GestureDetector(
+              //         onTap: () {print("entering a post");},
+              //         child: Container(
+              //           width: 120,
+              //           height: 120,
+              //           color: Colors.lightBlueAccent,
+              //         ),
+              //       );
+              //     },
+              //   ),
+              // ),
               Container(
                   // child: ListView.builder(),
                   ),
--- lib/widgets/pool_posts.dart
+++ lib/widgets/pool_posts.dart
@@ -10,30 +10,27 @@ class _PoolPostsState extends State<PoolPosts> {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 10),
-      child: Column(
-        children: [
-          Text("Top Posts"),
-          Container(
-            child: ListView.builder(
-              //spacing to the left
-              padding: EdgeInsets.only(left: 10.0),
-              itemCount: 5,
-              itemBuilder: (BuildContext context, int index) {
-                return GestureDetector(
-                  onTap: () {
-                    print("entering a post");
-                  },
-                  child: Container(
-                    width: 120,
-                    height: 120,
-                    color: Colors.lightBlueAccent,
-                  ),
-                );
-              },
+      child: Column(children: [
+      Text("Top Posts"),
+       Container(
+              child: ListView.builder(
+                //spacing to the left
+                padding: EdgeInsets.only(left:10.0),
+                itemCount: 5,
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                    onTap: () {print("entering a post");},
+                    child: Container(
+                      width: 120,
+                      height: 120,
+                      color: Colors.lightBlueAccent,
+                    ),
+                  );
+                },
+              ),
             ),
-          ),
-        ],
-      ),
+      ],),
+    
     );
   }
 }
