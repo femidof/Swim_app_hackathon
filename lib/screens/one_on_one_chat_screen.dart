23de49f3589e10ed449a770150cc473745ff1ca9
marrodri <46203279+marrodri@users.noreply.github.com>
2020-07-31 23:43:15 -0500

    adding category files

---
+++ lib/screens/one_on_one_chat_screen.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class OneOnOneChatScreen extends StatefulWidget {
+  @override
+  OneOnOne_ChatScreenState createState() => OneOnOne_ChatScreenState();
+}
+
+class OneOnOne_ChatScreenState extends State<OneOnOneChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
