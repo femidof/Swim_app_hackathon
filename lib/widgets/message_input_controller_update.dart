3bfc4129a5fecb64b65ca1a00edc29c5a73c0f86
marrodri <46203279+marrodri@users.noreply.github.com>
2020-08-02 12:10:24 -0500

    forgotten file

---
+++ lib/widgets/message_input_controller_update.dart
@@ -0,0 +1,183 @@
+import 'package:emoji_picker/emoji_picker.dart';
+import 'package:flutter/material.dart';
+
+import '../constant.dart';
+
+class MessageInputController extends StatefulWidget {
+  @override
+  _MessageInputControllerState createState() => _MessageInputControllerState();
+}
+
+class _MessageInputControllerState extends State<MessageInputController> {
+  TextEditingController textFieldController;
+  bool isWriting;
+  bool showEmojiPicker;
+  FocusNode textFieldFocus;
+
+  @override
+  void initState() {
+    super.initState();
+    textFieldController = TextEditingController();
+    isWriting = false;
+    showEmojiPicker = false;
+    textFieldFocus = FocusNode();
+  }
+
+  showKeyboard() => textFieldFocus.requestFocus();
+  hideKeyboard() => textFieldFocus.unfocus();
+
+  setWritingTo(bool val) {
+    setState(() {
+      isWriting = val;
+    });
+  }
+
+  showEmojiContainer() {
+    setState(() {
+      showEmojiPicker = true;
+    });
+  }
+
+  hideEmojiContainer() {
+    setState(() {
+      showEmojiPicker = false;
+    });
+  }
+
+  emojiContainer() {
+    return EmojiPicker(
+      rows: 3,
+      bgColor: Colors.white,
+      columns: 7,
+      indicatorColor: MAIN_COLOR,
+      onEmojiSelected: (emoji, category) {
+        // null;
+        setState(() {
+          isWriting = true;
+        });
+        textFieldController.text = textFieldController.text + emoji.emoji;
+      },
+      recommendKeywords: ["face", "happy", "party", "sad"],
+      numRecommended: 50,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: Row(
+        children: <Widget>[
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              padding: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                // gradient: MAIN_GRAD,
+                shape: BoxShape.circle,
+              ),
+              child: GestureDetector(
+                onTap: () {},
+                child: Icon(
+                  Icons.attach_file,
+                  color: Colors.black54,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 5,
+          ),
+          Expanded(
+            child: Stack(
+              alignment: Alignment.centerRight,
+              children: <Widget>[
+                TextField(
+                  controller: textFieldController,
+                  focusNode: textFieldFocus,
+                  onTap: () => hideEmojiContainer(),
+                  style: TextStyle(
+                    color: Colors.black54,
+                  ),
+                  onChanged: (val) {
+                    (val.length > 0 && val.trim() != "")
+                        ? setWritingTo(true)
+                        : setWritingTo(false);
+                  },
+                  decoration: InputDecoration(
+                    suffixIcon: Icon(Icons.ac_unit),
+                    hintText: "Type a message",
+                    hintStyle: TextStyle(
+                      color: MAIN_COLOR,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(
+                        const Radius.circular(50.0),
+                      ),
+                      borderSide: BorderSide(color: MAIN_COLOR, width: 3),
+                    ),
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    filled: true,
+                    fillColor: Colors.white,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    if (!showEmojiPicker) {
+                      hideKeyboard();
+                      showEmojiContainer();
+                    } else {
+                      showKeyboard();
+                      hideEmojiContainer();
+                    }
+                  },
+                  icon: Icon(
+                    Icons.face,
+                    color: Colors.transparent,
+                  ),
+                  highlightColor: Colors.transparent,
+                  splashColor: Colors.transparent,
+                ),
+              ],
+            ),
+          ),
+          isWriting
+              ? Container()
+              : Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Icon(
+                    Icons.mic,
+                    color: Colors.black54,
+                  ),
+                ),
+          isWriting
+              ? Container()
+              : GestureDetector(
+                  onTap: () {},
+                  // pickImage-----ImageSource.camera,
+                  child: Icon(
+                    Icons.camera_alt,
+                    color: Colors.black54,
+                  ),
+                ),
+          isWriting
+              ? Container(
+                  margin: EdgeInsets.only(left: 10),
+                  decoration: BoxDecoration(
+                    gradient: MAIN_GRAD,
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.send,
+                      size: 15,
+                    ),
+                    onPressed: () {},
+                  ))
+              : Container()
+        ],
+      ),
+    );
+  }
+}
